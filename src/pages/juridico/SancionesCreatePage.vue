9c4bf4170c6d166dd6f09533e283db796c7d7cd0
igonzalezg1 <ivan.gonzales@b-process.mx>
2025-05-12 15:02:19 -0600

    update

--- src/pages/juridico/SancionesCreatePage.vue
+++ src/pages/juridico/SancionesCreatePage.vue
@@ -4,110 +4,178 @@
       <form>
         <div class="row q-mx-md">
           <div class="col-12">
-            <p class="text-xl">INCIDENCIA</p>
+            <p class="tw-text-2xl">INCIDENCIA</p>
           </div>
         </div>
         <div class="row">
           <div class="col-6">
-            <q-select
-              filled
-              v-model="dataForm.tipo_incidencia"
-              label="Tipo de incidencia"
-              type="number"
-              class="q-ma-md"
-            />
-            <q-input
-              filled
-              readonly
-              v-model="dataForm.folio_incidencia"
-              label="Folio de incidencia"
-              type="text"
-              class="q-ma-md"
-            />
+            <p class="tw-text-lg q-mx-md tw-uppercase">
+              <span class="tw-font-semibold">Tipo de incidencia: </span
+              >{{ dataForm.tipo_incidencia }}
+            </p>
+            <p class="tw-text-lg q-mx-md tw-uppercase tw-font-semibold">
+              <span class="tw-font-semibold">Folio de incidencia:</span>
+              {{ dataForm.folio_incidencia }}
+            </p>
           </div>
           <div class="col-6">
-            <q-input
-              filled
-              readonly
-              v-model="dataForm.fecha_incidente"
-              label="Fecha y Hora de incidente"
-              type="text"
-              class="q-ma-md"
-            />
-            <q-input
-              filled
-              readonly
-              v-model="dataForm.personal_custodia"
-              label="Personal que custodia"
-              type="text"
-              class="q-ma-md"
-            />
+            <p class="tw-text-lg q-mx-md tw-uppercase tw-font-semibold">
+              <span class="tw-font-semibold">Fecha y Hora de incidente: </span
+              >{{ dataForm.fecha_incidente }}
+            </p>
+            <p class="tw-text-lg q-mx-md tw-uppercase tw-font-semibold">
+              <span class="tw-font-semibold">Personal que custodia: </span
+              >{{ dataForm.personal_custodia }}
+            </p>
           </div>
         </div>
         <div class="row">
           <div class="col-12">
-            <q-input
-              filled
-              readonly
-              v-model="dataForm.descripcion_incidencia"
-              label="Descripción de la incidencia"
-              type="text"
-              class="q-ma-md"
-            />
+            <p class="tw-text-lg q-mx-md tw-uppercase tw-font-semibold">
+              <span class="tw-font-semibold">Descripción de la incidencia: </span
+              >{{ dataForm.descripcion_incidencia }}
+            </p>
           </div>
         </div>
 
         <div class="row q-mx-md">
           <div class="col-12">
-            <p class="text-xl">SANCION</p>
+            <p class="tw-text-2xl">SANCION</p>
           </div>
         </div>
-        <div class="row">
+        <div class="row tw-uppercase">
           <div class="col-6">
             <q-select
               filled
-              v-model="dataForm.tipo_incidencia"
-              label="Tipo de incidencia"
+              v-model="dataForm.tipo_sancion_id"
+              label="Tipo de sanción"
               type="number"
               class="q-ma-md"
             />
             <q-input
               filled
-              readonly
-              v-model="dataForm.folio_incidencia"
-              label="Folio de incidencia"
-              type="text"
+              v-model="dataForm.fecha_hora_inicio_sancion"
+              label="Fecha inicio de sanción"
+              type="date"
+              class="q-ma-md"
+            />
+            <q-input
+              filled
+              v-model="dataForm.fecha_hora_fin_sancion"
+              label="Fecha fin de sanción"
+              type="date"
+              class="q-ma-md"
+            />
+            <q-input
+              filled
+              v-model="dataForm.fecha_hora_fin_sancion"
+              label="Lugar de aplicacion de la sanción"
+              type="textarea"
               class="q-ma-md"
             />
           </div>
           <div class="col-6">
             <q-input
               filled
-              readonly
               v-model="dataForm.fecha_incidente"
-              label="Fecha y Hora de incidente"
+              label="Fecha de sanción"
               type="text"
               class="q-ma-md"
             />
             <q-input
               filled
-              readonly
               v-model="dataForm.personal_custodia"
-              label="Personal que custodia"
+              label="Fecha real de fin de sanción"
               type="text"
               class="q-ma-md"
+            />
+            <q-input
+              filled
+              v-model="dataForm.dias_sancion"
+              label="Días de sanción"
+              type="number"
+              class="q-ma-md"
+            />
+            <q-input
+              filled
+              v-model="dataForm.descripcion"
+              label="Descripción de la sanción"
+              type="textarea"
+              class="q-ma-md tw-uppercase"
             />
           </div>
         </div>
-        <div class="row">
-          <div class="col-12">
+        <div class="row tw-uppercase">
+          <div class="col-12">Involucrados</div>
+          <table class="q-table">
+            <thead class="text-center bg-primary text-white">
+              <tr>
+                <th>#</th>
+                <th>PPL</th>
+                <th>Expediente</th>
+                <th>Tipo de participación</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Nombre PPL</td>
+                <td>Expediente</td>
+                <td>Tipo de participación</td>
+                <td>
+                  <q-toggle v-model="dataForm.firmante_2_cargo" color="primary" class="q-ma-md" />
+                </td>
+              </tr>
+              <!-- <tr v-for="(involucrado, index) in dataForm.involucrados" :key="index">
+                <td>{{ involucrado.nombre }}</td>
+                <td>{{ involucrado.cargo }}</td>
+                <td>
+                  <q-btn
+                    color="negative"
+                    icon="delete"
+                    @click="dataForm.involucrados.splice(index, 1)"
+                  />
+                </td>
+              </tr> -->
+            </tbody>
+          </table>
+        </div>
+        <div class="row tw-uppercase">
+          <div class="col-3">
             <q-input
               filled
-              readonly
-              v-model="dataForm.descripcion_incidencia"
-              label="Descripción de la incidencia"
+              v-model="dataForm.firmante_1_nombre"
+              label="Nombre del firmante 1"
               type="text"
-              class="q-ma-md"
+              class="q-ma-sm"
+            />
+          </div>
+          <div class="col-3">
+            <q-input
+              filled
+              v-model="dataForm.firmante_1_cargo"
+              label="Cargo del firmante 1"
+              type="text"
+              class="q-ma-sm"
+            />
+          </div>
+          <div class="col-3">
+            <q-input
+              filled
+              v-model="dataForm.firmante_2_nombre"
+              label="Nombre del firmante 2"
+              type="text"
+              class="q-ma-sm"
+            />
+          </div>
+          <div class="col-3">
+            <q-input
+              filled
+              v-model="dataForm.firmante_2_cargo"
+              label="Cargo del firmante 2"
+              type="text"
+              class="q-ma-sm"
             />
           </div>
         </div>
@@ -124,7 +192,7 @@
       />
     </div>
   </div>
-  <div class="row">
+  <div class="row" v-if="sancionId > 0">
     <div class="col-12">
       <q-stepper v-model="step" vertical color="primary" animated>
         <q-step :name="1" title="OPCIONES DE SANCION" icon="settings" :done="step > 1">
@@ -239,7 +307,11 @@ const dataForm: SancionCreate = {
   firmante_2_cargo: '',
 };
 onMounted(() => {
-  sancionId.value = Number(route.query.incidenciaId);
+  if (route.query.incidenciaId) {
+    sancionId.value = Number(route.query.incidenciaId);
+  } else {
+    sancionId.value = 0;
+  }
 });
 </script>
 <style scoped></style>
