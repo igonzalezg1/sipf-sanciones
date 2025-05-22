3fb6e1b5a7178bb2396066dc15878720a309b2b5
dev.generico7 <dev.generico7@gmail.com>
2025-05-22 17:52:54 -0600

    Actualizando la configuración de Quasar: agregando variables HTML, modificando el modo de enrutador a 'history', y añadiendo plugins adicionales.

--- quasar.config.ts
+++ quasar.config.ts
@@ -8,10 +8,16 @@ export default defineConfig((/* ctx */) => {
     // https://v2.quasar.dev/quasar-cli-vite/prefetch-feature
     // preFetch: true,
 
+    //Titutlo de la pestaña
+    htmlVariables: {
+      productName: 'Sistema Integral Penitenciario Federal',
+      productDescription: 'Sistema Integral Penitenciario Federal',
+    },
+
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
-    boot: ['axios'],
+    boot: ['axios', 'permiso', 'global-loader'],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#css
     css: ['app.scss'],
@@ -32,6 +38,7 @@ export default defineConfig((/* ctx */) => {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#build
     build: {
+      publicPath: '/sanciones/',
       target: {
         browser: ['es2022', 'firefox115', 'chrome115', 'safari14'],
         node: 'node20',
@@ -43,7 +50,7 @@ export default defineConfig((/* ctx */) => {
         // extendTsConfig (tsConfig) {}
       },
 
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
@@ -82,13 +89,6 @@ export default defineConfig((/* ctx */) => {
       // https: true,
       port: 9000,
       open: true, // opens browser window automatically
-      proxy: {
-        '/sipf/': {
-          target: 'http://localhost:4200',
-          changeOrigin: true,
-          rewrite: (path: string) => path.replace(/^\/sipf/, ''),
-        },
-      },
     },
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#framework
@@ -96,7 +96,7 @@ export default defineConfig((/* ctx */) => {
       config: {},
 
       // iconSet: 'material-icons', // Quasar icon set
-      // lang: 'en-US', // Quasar language pack
+      lang: 'es', // Quasar language pack
 
       // For special cases outside of where the auto-import strategy can have an impact
       // (like functional components as one of the examples),
@@ -106,7 +106,7 @@ export default defineConfig((/* ctx */) => {
       // directives: [],
 
       // Quasar plugins
-      plugins: ['Notify'],
+      plugins: ['Notify', 'Dialog'],
     },
 
     // animations: 'all', // --- includes all animations
