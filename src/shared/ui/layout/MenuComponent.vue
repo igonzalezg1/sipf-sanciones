1f073e1c3276874cd3cad28bd62efd2cd207904a
dev.generico7 <dev.generico7@gmail.com>
2025-05-22 17:55:46 -0600

    Eliminar componente `MenuComponent`: se ha eliminado el archivo `MenuComponent.vue` de la interfaz compartida.

--- src/shared/ui/layout/MenuComponent.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <q-tabs align="center" shrink class="text-white q-ml-xl">
-    <q-route-tab to="/sanciones-tecnico" label="Sanciones - técnico" />
-    <q-route-tab to="/sanciones-juridico" label="Sanciones - jurídico" />
-  </q-tabs>
-</template>
-
-<script setup lang="ts"></script>
